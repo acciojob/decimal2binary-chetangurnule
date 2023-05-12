1b1cd0a24cd226f0a20761cb564bc2b0985dbbec
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-12 21:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function threeSum(arr, target) {
 // write your code here
-  
+	let div = document.querySelector(".div");
+	for(let i = 0; i < arr.length; i++){
+		div.textContent += arr[i] + " ";
+	}
 }
 
 module.exports = threeSum;
