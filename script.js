40b870234cc19d6af514e389f217244ee2a56250
Chetan Gurnule <105580754+chetangurnule@users.noreply.github.com>
2023-05-14 08:49:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function threeSum(arr, target) {
-// write your code here
-	let div = document.querySelector(".div");
-	for(let i = 0; i < arr.length; i++){
-		div.textContent += arr[i] + " ";
-	}
+function decimalToBinary(number){
+	return parseInt(number).toString(2);
 }
-
 module.exports = threeSum;
